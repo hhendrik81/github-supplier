542bed33633a8faec2c774751b6aed8eb04e61ff
hhendrik81 <hhouinou@gmail.com>
2021-10-29 15:39:30 +0200

    3 files modified

--- src/components/SuppliersMap.vue
+++ src/components/SuppliersMap.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
     <h1>Carte des Fournisseurs</h1>
+    <l-map style="height: 300px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng"></l-marker>
+    </l-map>
   </div>
 </template>
 
 <script>
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
 export default {
   name: 'SuppliersMap',
+
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 15,
+      center: [51.505, -0.159],
+      markerLatLng: [51.504, -0.159]
+    };
+  }
 };
 </script>
 
